1639b0c15d4f135fb3a93740c5fc13bb9796ce84
MacFlurry <omegabk@gmail.com>
2019-12-12 14:57:57 +0100

    adding some text

--- lambda/hello_world.py
+++ lambda/hello_world.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello from Lambda by Omega!')
     }
 
